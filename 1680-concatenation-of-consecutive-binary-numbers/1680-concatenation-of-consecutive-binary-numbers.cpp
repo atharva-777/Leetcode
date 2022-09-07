54a8880ac651baa2ee507033fc9cd1b74010c040
atharva-777 <111676983+atharva-777@users.noreply.github.com>
2022-09-07 10:04:30 +0530

    Time: 511 ms (29.07%), Space: 5.8 MB (97.54%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+   int dectobin(int arr[17],int n){
+       int i=0;
+       while(n){
+           arr[i++] = n%2;
+           n>>=1;
+       }
+       return i;
+   }
+    
+    int concatenatedBinary(int n) {
+        unsigned long long int ans = 0;
+        unsigned long long int M = 1e9+7;
+        
+        for(int i=1;i<=n;i++){
+            int bits[17];
+            int j = dectobin(bits,i);
+            while(j){
+                ans = (ans*2)%M+bits[--j];
+            }
+        }
+        return ans;
+    }
+};
